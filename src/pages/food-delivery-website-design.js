77e5c7e5e6ff9121062a70fedf5c73fd673e855a
Abhas Rawat <arabhasrawat@gmail.com>
2023-03-01 13:04:23 +0530

    first commit

---
+++ src/pages/food-delivery-website-design.js
@@ -0,0 +1,73 @@
+import "./food-delivery-website-design.css";
+
+const FoodDeliveryWebsiteDesign = () => {
+  return (
+    <div className="food-delivery-website-design">
+      <div className="desktop-1">
+        <b className="bhukkad">Bhukkad</b>
+        <div className="account">
+          <div className="account1">Account</div>
+          <img className="vector-icon" alt="" src="../vector.svg" />
+        </div>
+        <div className="about-us">About Us</div>
+        <div className="bag-icon">
+          <img className="vector-icon1" alt="" src="../vector1.svg" />
+          <div className="ellipse-parent">
+            <img className="group-child" alt="" src="../ellipse-2.svg" />
+            <b className="b">0</b>
+          </div>
+        </div>
+        <div className="confirm-order">
+          <div className="confirm-order-child" />
+          <div className="confirm-order1">Confirm Order</div>
+        </div>
+        <div className="washington">
+          <img className="map-icon" alt="" src="../map-icon.svg" />
+          <div className="chandigarh-university">Chandigarh University</div>
+        </div>
+        <b className="your-favorite-meals">
+          Your favorite meals, just a few clicks away
+        </b>
+        <div className="food">
+          <img className="food-child" alt="" src="../rectangle-7@2x.png" />
+          <div className="food-item" />
+          <div className="food1">Amul</div>
+          <img className="vector-icon2" alt="" src="../vector2.svg" />
+        </div>
+        <div className="pizza">
+          <img className="food-child" alt="" src="../rectangle-8@2x.png" />
+          <div className="pizza-item" />
+          <img className="vector-icon3" alt="" src="../vector3.svg" />
+          <div className="mcb">M.C.B</div>
+        </div>
+        <div className="healthy">
+          <img className="healthy-child" alt="" src="../rectangle-9@2x.png" />
+          <div className="healthy-item" />
+          <img className="vector-icon4" alt="" src="../vector4.svg" />
+          <div className="healthy1">Healthy</div>
+        </div>
+        <div className="lemon1-7455a79b5bf64182a5b00cc" />
+        <div className="what-about-privecy-policy">
+          <img className="what-about-privecy-policy-child" alt="" />
+          <img className="vector-icon5" alt="" src="../vector5.svg" />
+          <div className="frame">
+            <img className="vector-icon6" alt="" src="../vector6.svg" />
+            <img className="vector-icon7" alt="" src="../vector7@2x.png" />
+            <img className="vector-icon8" alt="" src="../vector8.svg" />
+            <img className="vector-icon9" alt="" src="../vector9.svg" />
+            <div className="food2">Amul</div>
+          </div>
+        </div>
+        <div className="frame1">
+          <img className="vector-icon6" alt="" src="../vector10.svg" />
+          <img className="vector-icon7" alt="" src="../vector11@2x.png" />
+          <img className="vector-icon12" alt="" src="../vector12.svg" />
+          <img className="vector-icon2" alt="" src="../vector13.svg" />
+          <div className="food3">Juice bar</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FoodDeliveryWebsiteDesign;
